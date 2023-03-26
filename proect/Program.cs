46f40e05e7399035090275d358f6ba7f5a763689
AVShanaev <AVShanaev@gmail.com>
2023-03-26 17:20:55 +0300

    add_cs_input/output_method

--- proect/Program.cs
+++ proect/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача:
+
+   Написать программу, которая из имеющегося массива строк сформирует массив строк
+   длинна которых меньше либо равна 3 символа. 
+   Первоначальный массив можно ввести с клавиатуры, либо задать на страте выполнения алгоритма. 
+   При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] FillArray(int length)
+{
+   string[] resultArray = new string[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.Write($"Укажите значение {i + 1}  строки: ");
+        resultArray[i] = System.Console.ReadLine();
+        System.Console.WriteLine();
+    }
+
+    return resultArray;
+}
+
+void ShowArray(string[] array)
+{
+    //Вывод с помощью foreach
+    foreach (var elem in array)
+    {
+        Console.Write("{0} ", elem);
+    }
+
+    Console.WriteLine();
+}
